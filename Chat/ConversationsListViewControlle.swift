715dd59df0487464480be923713a7d3878e6a742
simonkrivonosov <simon.krivonosov@gmail.com>
2019-05-29 13:59:58 +0300

    mistake in pushing

--- Chat/ConversationsListViewControlle.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ConversationsListViewControlle.swift
-//  Chat
-//
-//  Created by Семен Кривоносов on 08/10/2018.
-//  Copyright © 2018 Семен Кривоносов. All rights reserved.
-//
-
-import Foundation
